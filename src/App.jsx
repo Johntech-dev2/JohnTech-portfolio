3d7b3d3549a0f2ac8f6fa005812cb4f48d43c0b4
johntech-dev <johnayomide50@gmail.com>
2023-08-11 15:45:28 +0100

    'john'

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Profile from "./Component/Home/Profile";
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Home/Profile.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import Typical from "react-typical";
+
+function Profile() {
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <a href="https://web.facebook.com/profile.php?id=100089760934929" target="_blank" rel="noopener"> 
+              <i className="fa fa-facebook-square" />
+            </a>
+            <a href="https://www.instagram.com/alinmolayanjohn/" target="_blank" rel="noopener">
+              <i class="bi bi-instagram" />
+            </a>
+            <a href="https://twitter.com/johnayomide50" target="_blank" rel="noopener">
+              <i className="fa fa-twitter-square" />
+            </a>
+            <a href="https://www.linkedin.com/in/john-tech-742782275/" target="_blank" rel="noopener">
+              <i class="bi bi-linkedin" />
+            </a>
+          </div>
+      </div>
+      <div className="profile-details-name">
+        <h4>Hello, I'M <span className="highlighted-text">John</span></h4>
+      </div>
+      <div className="profile-details-role">
+        <span className="primary-text">
+          {""}
+           <h1>
+            {""}
+            <Typical 
+            loop={Infinity}
+            steps={[
+              "Ethusistic Dev 😎",
+              1000,
+              "React developer 💻",
+              1000,
+              "Junior React developer 💻",
+              1000,
+              "Web developer 💻",
+              1000,
+            ]}
+            
+            />
+           </h1>
+           <span className='profile-tag-line'>
+            Knack of building applications with front end operations.
+            </span>
+        </span>
+      </div>
+      <div className='profile-options'>
+        <button className='btn primary-btn'>
+        {""}
+        Hire Me{""}
+        </button>
+        <a href='John Resume.pdf' download="John Resume.pdf">
+            <button className='btn highlighted-btn'>
+                Get Resume
+            </button>
+        </a>
+      </div>
+    </div>
+    </div>
+  );
+}
+
+export default Profile;
