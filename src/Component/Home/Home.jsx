2fa70228b801626c44251952377199900bfe2f6a
johntech-dev <johnayomide50@gmail.com>
2023-09-05 00:12:23 +0100

    upadte

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -2,13 +2,11 @@ import React from 'react'
 import Typical from 'react-typical'
 function Home() {
   return (
-    <div>
        <div>  
           <h1 className='text-[35px] font-medium md:text-[30px] ml-14' >Hello,</h1>
-       </div>
        <div className='flex'>
-         <h1 className='text-[35px] font-medium md: ml-14 flex-wrap'>I am a</h1>
-          <h1 className='text-[40px] font-medium text-purple-600'>
+         <h1 className='text-[35px] font-medium md: ml-14 whitespace-normal'>I am a</h1>
+          <h1 className='text-[40px] font-medium text-purple-600 md:whitespace-normal'>
            <Typical 
            loop={Infinity}
            steps={[
@@ -26,7 +24,21 @@ function Home() {
            />
           </h1>
        </div>
+       
+      <p className='text-gray-500 ml-14 w-96 md: whitespace-normal'>I am John tech i am a front-end react web developer and  With a passion for creating innovative and user-friendly websites.</p> 
+      <br />
+      <div>
+      <a href='John Resume.pdf' download='John Resume.pdf'>
+      <button className='hover:z-50 transition-all duration-300 ease-in-out  hover:scale-125 bg-purple-600 p-2 rounded-md text-white ml-14 mr-14' >
+         Resume
+      </button>
+      </a>
+      <a href='#'>
+         <button className='hover:z-50 transition-all duration-300 ease-in-out hover:scale-125 bg-purple-600 p-2 rounded-md text-white'>Hire me</button>
+      </a>
+      </div>
     </div>
+    
   )
 }
 
