444a2a29e0af8595a1044e4ba2b306a36037263e
johntech-dev <johnayomide50@gmail.com>
2023-09-04 23:13:17 +0100

    upadte

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -3,21 +3,22 @@ import Typical from 'react-typical'
 function Home() {
   return (
     <div>
-       <div className='text-xl'>  
-          Hello, I'M
+       <div>  
+          <h1 className='text-[35px] font-medium md:text-[30px] ml-14' >Hello,</h1>
        </div>
-       <div>
-          <h1 className='text-4xl'>
+       <div className='flex'>
+         <h1 className='text-[35px] font-medium md: ml-14 flex-wrap'>I am a</h1>
+          <h1 className='text-[40px] font-medium text-purple-600'>
            <Typical 
            loop={Infinity}
            steps={[
-             "Ethusiastic developer",
+             "Ethusiastic dev",
               1000,
              "React Developer",
               1000,
              "Web developer",
               1000,
-             "Web app developer",
+             "Web app dev",
               1000,
              "React Native",
               1000,
